9ebca712f920b8bb4573b1fd6197ff2f138d0cdc
Zty <710979777@qq.com>
2016-10-24 20:23:56 +0800

    #1、修复bspatch.c文件上次提交被格式化后无法编译的问题； #2、修改[app_v2]直接使用.so文件编译； #3、隐藏签名信息；

--- app_v1/src/main/jni/bspatch.c
+++ app_v1/src/main/jni/bspatch.c
@@ -37,42 +37,25 @@ __FBSDID("$FreeBSD: src/usr.bin/bsdiff/bspatch/bspatch.c,v 1.1 2005/08/06 01:59:
 #include <fcntl.h>
 #include <jni.h>
 
-JNIEXPORT jintJNICALL
-Java_com_zty_utils_AppUtil_bspatch
-(JNIEnv
-*env,
-jclass cls,
-        jstring
-old,
-jstring new, jstring
-patch){
-int argc = 4;
-char *argv[argc];
-argv[0] = "bspatch";
-argv[1] = (char *) ((*env)->
-GetStringUTFChars(env, old,
-0));
-argv[2] = (char *) ((*env)->
-GetStringUTFChars(env, new,
-0));
-argv[3] = (char *) ((*env)->
-GetStringUTFChars(env, patch,
-0));
 
 
-int ret = patchMethod(argc, argv);
+JNIEXPORT jint JNICALL Java_com_zty_utils_AppUtil_bspatch
+        (JNIEnv *env, jclass cls,
+         jstring old, jstring new, jstring patch){
+    int argc = 4;
+    char * argv[argc];
+    argv[0] = "bspatch";
+    argv[1] = (char*) ((*env)->GetStringUTFChars(env, old, 0));
+    argv[2] = (char*) ((*env)->GetStringUTFChars(env, new, 0));
+    argv[3] = (char*) ((*env)->GetStringUTFChars(env, patch, 0));
 
-(*env)->
-ReleaseStringUTFChars(env, old, argv[1]
-);
-(*env)->
-ReleaseStringUTFChars(env, new, argv[2]
-);
-(*env)->
-ReleaseStringUTFChars(env, patch, argv[3]
-);
-return
-ret;
+
+    int ret = patchMethod(argc, argv);
+
+    (*env)->ReleaseStringUTFChars(env, old, argv[1]);
+    (*env)->ReleaseStringUTFChars(env, new, argv[2]);
+    (*env)->ReleaseStringUTFChars(env, patch, argv[3]);
+    return ret;
 }
 
 static off_t offtin(u_char *buf) {
